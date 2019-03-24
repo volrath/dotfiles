0ca2592d6ee1178f6c8f813f5672a3a979be6364
Daniel Barreto <daniel.barreto.n@gmail.com>
2019-03-24 13:36:06 +0100

    Add initial version of dotfiles installer

---
+++ install.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+# CREDITS: This script is based on Zach Holman's work
+# (https://github.com/holman/dotfiles/blob/master/script/bootstrap), with some
+# minor modifications
+
+set -euo pipefail
+
+function info () {
+    printf "  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+function user () {
+    printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+function success () {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+
+function link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $(basename "$src"), what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "Removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "Moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "Skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "Linked $1 to $2"
+  fi
+}
+
+
+function link_dotfiles () {
+  info 'Installing dotfiles'
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  dotfiles=$(find `pwd` -maxdepth 1 -name '.*' -not -name '.git')
+  for src in $dotfiles; do
+    dst="$HOME/$(basename "${src}")"
+    link_file "$src" "$dst"
+  done
+}
+
+
+function install_prelude() {
+    if [ ! -d "${HOME}/.emacs.d" ]; then
+        wget --no-check-certificate https://github.com/bbatsov/prelude/raw/master/utils/installer.sh -O - | sh;
+    fi
+}
+
+function install_oh_my_zsh() {
+    if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+        curl -L http://install.ohmyz.sh | sh
+    fi
+}
+
+
+install_prelude
+install_oh_my_zsh
+link_dotfiles
+
+echo ''
+info '=> All installed!'
